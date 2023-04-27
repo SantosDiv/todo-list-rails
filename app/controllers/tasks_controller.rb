b0766871dd678492802e86039b32c19f4e3952db
Diogenes Santos <diogenesfelipe.s@outlook.com>
2023-04-27 17:54:45 -0300

    Criando controller de task

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,46 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:update, :destroy]
+
+  def index
+    @tasks = Task.only_parent.order(:date, :asc)
+  end
+
+  def new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    begin
+      @task.save!
+      redirect_to tasks_path, notice: "Tarefa criada com sucesso", status: :ok
+    rescue => exception
+      redirect_to new_task_path, alert: exception.message, status: :bad_request
+    end
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: "Tarefa atualizada com sucesso", status: :ok
+    else
+      render :index, alert: "Ocorre um erro ao atualizar a tarefa", status: :bad_request
+    end
+  end
+
+  def destroy
+    if @task.destroy!
+      redirect_to tasks_path, notice: "Tarefa deletada com sucesso", status: :ok
+    else
+      render :index, alert: "Ocorre um erro ao deletar a tarefa", status: :bad_request
+    end
+  end
+
+  private
+
+  def set_task
+    @task - Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:description, :date, :done)
+  end
+end
