fb2fd8fb48ad2fd34d1eb0eceff10124130a7369
Diogenes Santos <diogenesfelipe.s@outlook.com>
2023-04-28 17:08:44 -0300

    Criando casos de testes para o destroy e update

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe TasksController, type: :controller do
       it 'create a task and return status 201' do
         post :create, params: params
 
-        expect(Task.all.count).to eq(1)
+        expect(Task.count).to eq(1)
         expect(response.status).to eq(201)
       end
     end
@@ -34,7 +34,7 @@ RSpec.describe TasksController, type: :controller do
       it 'not create task and return status 400' do
         delete :create, params: params
 
-        expect(Task.all.count).to eq(0)
+        expect(Task.count).to eq(0)
         expect(response.status).to eq(400)
       end
     end
@@ -56,12 +56,71 @@ RSpec.describe TasksController, type: :controller do
         }
       }
 
-      it 'create a task and return status 201' do
+      it 'update a task and return status 200' do
         put :update, params: params
-        task = Task.last
+
+        task.reload
+        expect(response.status).to eq(200)
         expect(task.description).to eq(description)
         expect(task.done).to eq(true)
-        expect(response.status).to eq(201)
+      end
+    end
+
+    context 'when task params is invalid' do
+      let(:task) { create(:task) }
+      let(:description) { "" }
+
+      let(:params) {
+        {
+          id: task.id,
+          task: {
+            description: description,
+            date: Time.zone.now,
+            done: nil
+          }
+        }
+      }
+
+      it 'does not a update task and return status 400' do
+        put :update, params: params
+
+        task.reload
+        expect(task.description).to eq(task.description)
+        expect(task.done).to eq(false)
+        expect(response.status).to eq(400)
+      end
+    end
+  end
+
+  describe "#destroy" do
+    context 'when task params is correctly' do
+      let(:task) { create(:task) }
+
+      let(:params) {
+        {
+          id: task.id
+        }
+      }
+
+      it 'destroy a task and return status 200' do
+        delete :destroy, params: params
+
+        expect(response.status).to eq(200)
+        expect(Task.count).to eq(0)
+      end
+    end
+
+    context 'when task does not exist' do
+      let(:params) {
+        {
+          id: "9999999"
+        }
+      }
+
+      it 'return status 400' do
+        delete :destroy, params: params
+
+        expect(response.status).to eq(400)
       end
     end
   end
