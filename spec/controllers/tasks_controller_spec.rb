e664ca8823d1f73fb2906e07af27ef700158ba2f
Diogenes Santos <diogenesfelipe.s@outlook.com>
2023-04-29 10:34:36 -0300

    Adicionando teste para subtasks

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -2,40 +2,64 @@ require 'rails_helper'
 
 RSpec.describe TasksController, type: :controller do
   describe "#create" do
-    context 'when task params is correctly' do
-      let(:params) {
-        {
-          task: {
-            description: "Fazer compras",
-            date: Time.zone.now
+    describe 'create a task' do
+      context 'when task params is correctly' do
+        let(:params) {
+          {
+            task: {
+              description: "Fazer compras",
+              date: Time.zone.now
+            }
           }
         }
-      }
 
-      it 'create a task and return status 201' do
-        post :create, params: params
+        it 'create a task and return status 201' do
+          post :create, params: params
 
-        expect(Task.count).to eq(1)
-        expect(response.status).to eq(201)
+          expect(Task.count).to eq(1)
+          expect(response.status).to eq(201)
+        end
+      end
+
+      context 'when task params is invalid' do
+        let(:params) {
+          {
+            task: {
+              description: "",
+              date: Time.zone.now,
+              done: nil
+            }
+          }
+        }
+
+        it 'not create task and return status 400' do
+          delete :create, params: params
+
+          expect(Task.count).to eq(0)
+          expect(response.status).to eq(400)
+        end
       end
     end
 
-    context 'when task params is invalid' do
-      let(:params) {
-        {
-          task: {
-            description: "",
-            date: Time.zone.now,
-            done: nil
+    describe 'create a subtask' do
+      context 'when subtask params is correctly' do
+        let(:task) { create(:task) }
+        let(:params) {
+          {
+            task: {
+              description: "Fazer compras",
+              date: Time.zone.now,
+              parent_id: task.id
+            }
           }
         }
-      }
 
-      it 'not create task and return status 400' do
-        delete :create, params: params
+        it 'create a subtask and return status 201' do
+          post :create, params: params
 
-        expect(Task.count).to eq(0)
-        expect(response.status).to eq(400)
+          expect(Task.count).to eq(2)
+          expect(response.status).to eq(201)
+        end
       end
     end
   end
