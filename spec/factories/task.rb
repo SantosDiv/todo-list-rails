ee267cbfcff9fbd76120a933e523b502ec2b91b0
Diogenes Santos <diogenesfelipe.s@outlook.com>
2023-04-29 16:59:51 -0300

    Adicionando nova trait na factory de task de tasks com sub tasks

--- spec/factories/task.rb
+++ spec/factories/task.rb
@@ -6,5 +6,13 @@ FactoryBot.define do
     trait :done do
       done { true }
     end
+
+    trait :with_sub_task do
+      after(:create) do |task|
+        sub_task = create(:task)
+        sub_task.parent_id = task.id
+        sub_task.save
+      end
+    end
   end
 end
