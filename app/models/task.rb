860d7602bd389913605a7ef9456427b70b36dede
Diogenes Santos <diogenesfelipe.s@outlook.com>
2023-04-29 16:26:18 -0300

    Adicionando scopes para filtrar pelas datas

--- app/models/task.rb
+++ app/models/task.rb
@@ -7,6 +7,19 @@ class Task < ApplicationRecord
 
   scope :only_parent, -> { where(parent_id: nil) }
 
+  scope :filter_by_date, -> (start_date:, end_date:) { where(date: start_date..end_date, parent_id: nil) }
+
+  scope :only_today, -> {
+    where(date: Time.zone.today.beginning_of_day..Time.zone.today.end_of_day, parent_id: nil)
+    .order(date: :asc)
+  }
+
+  scope :only_tomorrow, -> {
+    where(date: Time.zone.tomorrow.beginning_of_day..Time.zone.tomorrow.end_of_day, parent_id: nil)
+    .order(date: :asc)
+  }
+
+
   def has_sub_task?
     !self.sub_tasks.nil?
   end
