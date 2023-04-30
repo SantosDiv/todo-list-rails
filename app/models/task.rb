4656e18ae5e32dd13c6eadba0044338b8478aeed
Diogenes Santos <diogenesfelipe.s@outlook.com>
2023-04-30 16:42:13 -0300

    Ajustando a ordem pelo campo date no has_many sub_tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,7 +3,7 @@ class Task < ApplicationRecord
   validates :done, inclusion: [true, false]
 
   belongs_to :parent, class_name: 'Task', optional: true
-  has_many :sub_tasks, class_name: 'Task', foreign_key: :parent_id, dependent: :destroy
+  has_many :sub_tasks, -> { order("date") }, class_name: 'Task', foreign_key: :parent_id, dependent: :destroy
 
   scope :only_parent, -> { where(parent_id: nil) }
 
@@ -18,7 +18,6 @@ class Task < ApplicationRecord
     where(date: Time.zone.tomorrow.beginning_of_day..Time.zone.tomorrow.end_of_day, parent_id: nil)
     .order(date: :asc)
   }
-
 
   def has_sub_task?
     !self.sub_tasks.empty?
